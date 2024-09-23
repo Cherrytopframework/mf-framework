ac4ac8bddacecd6c3bad3f3c71003ed5753b4316
M <woodward.michael.a@gmail.com>
2024-09-23 07:10:40 -0500

    Trying the rspack module federation 1.5 plugin to troubleshoot deployment

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -5,18 +5,21 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // const ReactRefreshPlugin = require('@rspack/plugin-react-refresh');
 const Dotenv = require('dotenv-webpack');
 // const NpmDtsPlugin = require('npm-dts-webpack-plugin');
-// const rspack = require('@rspack/core');
+const rspack = require('@rspack/core');
 
 const {
     ModuleFederationPlugin,
 } = require('@module-federation/enhanced/rspack');
-const path = require('path');
 
 
 export default defineConfig({
-    entry: './src/index.ts',
-    // entry: './src/boostrap.tsx',
-    // context: __dirname,
+    // entry: './src/index.ts',
+    context: __dirname,
+    entry: {
+      main: './src/index.ts',
+    },
+    target: "web",
+    watch: true,
     // output: {
     //   path: path.resolve(__dirname, 'dist'),
     //   filename: 'modules.bundle.js',
@@ -90,12 +93,9 @@ export default defineConfig({
                 // new HtmlWebpackPlugin(),
                 // new rspack.EnvironmentPlugin(['NODE_ENV', 'DEBUG']),
                 // new NpmDtsPlugin(),
-                new Dotenv({
-                  // path: './.env', // Path to .env file (this is the default)
-                  // safe: true, // load .env.example (defaults to "false" which does not use dotenv-safe)
-                }),
-                new ModuleFederationPlugin({
-                    name: 'app',
+                new Dotenv({}),
+                new rspack.container.ModuleFederationPlugin({
+                  name: 'app',
                     // remotes: {},
                     exposes: {
                       "./App": "./src/App/index.tsx",
@@ -140,6 +140,52 @@ export default defineConfig({
                       zustand: { singleton: true, requiredVersion: "^4.1.1" }, // Share Zustand to ensure single store instance
                   },
                 }),
+                // new ModuleFederationPlugin({
+                //     name: 'app',
+                //     // remotes: {},
+                //     exposes: {
+                //       "./App": "./src/App/index.tsx",
+                //       "./CherrytopFramework": "./src/Entry.tsx",
+                //       "./AuthProvider": "./src/components/custom/Auth/Auth3.tsx",
+                //       // app/AppProvider includes theme, alert, confirm, drawer providers
+                //       "./AppProvider": "./src/components/custom/providers/Providers.tsx",
+                //       "./AlertProvider": "./src/components/custom/providers/AlertProvider.tsx",
+                //       "./ConfirmProvider": "./src/components/custom/providers/Confirm/ConfirmProvider.tsx",
+                //       "./BottomNavigation": "./src/components/Mui/BottomNavigation/BottomNavigation.tsx",
+                //       "./Camera": "./src/components/custom/Camera/Camera.tsx",
+                //       "./ChatBox": "./src/components/custom/Chat/Chat.tsx",
+                //       "./ChatView": "./src/components/custom/Chat/ChatView.tsx",
+                //       "./ChartsContainer": "./src/components/custom/charts/ChartsWrapper.tsx",
+                //       "./DrawerContainer": "./src/components/Mui/Drawer/Drawer.tsx",
+                //       "./DateTimeLabel": "./src/components/custom/DateTimeLabel/DateTimeLabel.tsx",
+                //       './DisplayCard': './src/components/Mui/DisplayCard/DisplayCard.tsx',                        
+                //       "./FormContainer": "./src/components/custom/forms/FormContainer.tsx",
+                //       './List': './src/components/Mui/List/List.tsx',
+                //       "./MarkdownWrapper": "./src/components/custom/wrappers/MarkdownWrapper/MarkdownWrapper.tsx",
+                //       "./Navbar": "./src/components/Mui/Navbar/Navbar.tsx",
+                //       "./NavMenu": "./src/components/Mui/Navbar/NavMenu.tsx",
+                //       "./NotionDataWrapper": "./src/components/custom/NotionPage/NotionPage.tsx",
+                //       "./QueryWrapper": "./src/components/custom/wrappers/QueryWrapper/QueryWrapper.tsx",
+                //       "./ReusablePopover": "./src/components/custom/ReusablePopover/ReusablePopover.tsx",
+                //       "./ReusableTable": "./src/components/custom/charts/ReusableTable.tsx",
+                //       "./Tabs": "./src/components/Mui/Tabs/Tabs.tsx",
+                //       "./ThemeProvider": "./src/utilities/theme/index.ts",
+                //       "./utilities/queries": "./src/utilities/api/index.ts",
+                //       "./utilities/store": "./src/utilities/store/index.ts",
+                //       "./utilities/store/utilityStore": "./src/utilities/store/utilityStore.ts"
+                //     },
+                //     shared: {
+                //       react: {
+                //           singleton: true,
+                //           requiredVersion: "^18.3.1"
+                //       },
+                //       "react-dom": {
+                //           singleton: true,
+                //           requiredVersion: "^18.3.1"
+                //       },
+                //       zustand: { singleton: true, requiredVersion: "^4.1.1" }, // Share Zustand to ensure single store instance
+                //   },
+                // }),
                 // //todo Need to add code to check if environment is development
                 // new ReactRefreshPlugin()
             ],
@@ -150,12 +196,28 @@ export default defineConfig({
       //     directory: path.join(__dirname, "public"),
       // },
       // host: "./dist",
+      host: '0.0.0.0',
+      allowedHosts: [
+        'auto'
+      ],
+      client: {
+        logging: 'info',
+        overlay: true,
+        progress: true
+      },
+      hot: true,
       port: 8080,
       headers: {
-        // 'Access-Control-Allow-Origin': ['*', 'http://localhost:3000'],
-          "Access-Control-Allow-Origin": "*",
+          'Access-Control-Allow-Origin': ['*', 'http://localhost:3000'],
+          // "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
           "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-      }
+      },
+      // proxy: [
+      //   {
+      //     context: ['/api'],
+      //     target: 'http://localhost:5052',
+      //   },
+      // ],
   },
 });
